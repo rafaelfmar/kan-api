43815be90f719f186d21ac28450f2eee9da1a762
felipeseolin <felipe.seolin@hotmail.com>
2019-11-16 16:05:32 -0300

    Done Lists API

--- src/controllers/ListController.js
+++ src/controllers/ListController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+const HTTPCode = require('../utils/HTTPCode');
 const ListInit = require('../models/List');
 const CardInit = require('../models/Card');
 const BoardInit = require('../models/Board');
@@ -8,108 +9,64 @@ const List = mongoose.model('List');
 const Board = mongoose.model('Board');
 const Card = mongoose.model('Card');
 
-function validate(name, description, board) {
-  let errors = [];
-
-  if (!name || name.trim().length === 0) {
-    errors = [...errors, 'Um nome deve ser dado à lista.'];
-  }
-  if (name && name.length > 100) {
-    errors = [...errors, 'O nome deve ter ao máximo 100 caracteres.'];
-  }
-  if (name && name.length < 2) {
-    errors = [...errors, 'O nome deve ter ao mínimo 2 caracteres.'];
-  }
-  if (description && description.length > 250) {
-    errors = [...errors, 'A descrição deve ter ao máximo 250 caracteres.'];
-  }
-  if (!board) {
-    errors = [...errors, 'A lista deve pertencer a um quadro'];
-  }
-
-  return errors;
-}
-
 module.exports = {
   async index(req, res) {
-    const lists = await List.find()
-      .populate('cards');
-
-    return res.render('lists', {
-      title: 'Listas',
-      lists,
-    });
-  },
-  async show(req, res) {
-    const list = await List.findById(req.params.idlist);
-    return res.render('list.create.handlebars', {
-      title: `Editar: ${list.name}`,
-      boardId: req.params.idboard,
-      formAction: `/lists/${req.params.idlist}`,
-      list,
-    });
-  },
-  form(req, res) {
-    return res.render('list.create.handlebars', {
-      title: 'Nova lista',
-      boardId: req.params.id,
-      formAction: '/lists/new',
-    });
+    const lists = await List.find().populate('cards');
+    return res.json(lists);
   },
   async store(req, res) {
     // Find Board
     const board = await Board.findById(req.body._board);
-    // Validation
-    const errors = validate(req.body.name, req.body.description, board);
-    if (errors.length > 0) {
-      req.flash('error', errors);
-      return res.redirect('back');
-    }
     // Create List
     const list = await List.create(req.body);
+    // Verify if the list is saved
     if (!list) {
-      req.flash('error', ['Ocorreu um erro ao salvar a lista. Tente novamente.']);
-      return res.redirect('back');
+      return res.sendStatus(HTTPCode.BAD_REQUEST);
     }
     // Update Board
     board.lists.push(list);
     board.save();
 
-    return res.redirect(`/boards/${board._id}`);
+    return res.status(HTTPCode.CREATED).json(list);
+  },
+  async show(req, res) {
+    const list = await List.findById(req.params.id);
+    if (!list) {
+      res.sendStatus(HTTPCode.NOT_FOUND).json(list);
+    }
+    return res.json(list);
   },
   async update(req, res) {
-    // Find Board
-    const board = await Board.findById(req.body._board);
-    const listCheck = await List.findById(req.params.id);
-    // Validation
-    const errors = validate(req.body.name, req.body.description, board);
-    if (errors.length > 0) {
-      req.flash('error', errors);
-      return res.redirect('back');
-    }
-    if (!board._id.equals(listCheck.id)) {
-      req.flash('error', ['Não é possível trocar o quadro de uma lista.']);
-      return res.redirect('back');
-    }
     // Create List
     const list = await List.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
+    // Verifica se já salvou
     if (!list) {
-      req.flash('error', ['Ocorreu um erro ao salvar a lista. Tente novamente.']);
-      return res.redirect('back');
+      res.sendStatus(HTTPCode.BAD_REQUEST);
     }
-    return res.redirect(`/boards/${req.body._board}`);
+
+    return res.status(HTTPCode.OK).json(list);
   },
   async destroy(req, res) {
-    const list = await List.findByIdAndRemove(req.params.idlist);
-    await Card.deleteMany({ _list: req.params.idlist });
-
+    // Delete list
+    const list = await List.findByIdAndRemove(req.params.id);
+    if (!list) {
+      res.sendStatus(HTTPCode.BAD_REQUEST);
+    }
+    // Delete the cards
+    const cards = await Card.deleteMany({ _list: req.params.id });
+    if (!cards) {
+      res.sendStatus(HTTPCode.BAD_REQUEST);
+    }
     // Delete from board
     const board = await Board.findOne({ lists: list.id });
-    board.lists = board.lists.filter((item) => !item.equals(req.params.idlist));
-    board.save();
+    board.lists = board.lists.filter(item => !item.equals(req.params.id));
+    const boardRes = board.save();
+    if (!boardRes) {
+      res.sendStatus(HTTPCode.BAD_REQUEST);
+    }
 
-    return res.redirect(`/boards/${board.id}`);
+    return res.status(HTTPCode.OK).json(list);
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const BoardController = require('./controllers/BoardController');
+const ListController = require('./controllers/ListController');
 
 routes.get('/', (req, res) => res.send('Kan API'));
 
@@ -12,5 +13,11 @@ routes.post('/boards', BoardController.store);
 routes.get('/boards/:id', BoardController.show);
 routes.patch('/boards/:id', BoardController.update);
 routes.delete('/boards/:id', BoardController.destroy);
+// Lists
+routes.get('/lists', ListController.index);
+routes.post('/lists', ListController.store);
+routes.get('/lists/:id', ListController.show);
+routes.patch('/lists/:id', ListController.update);
+routes.delete('/lists/:id', ListController.destroy);
 
 module.exports = routes;
