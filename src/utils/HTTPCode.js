479e850b900ede636eedf2a8af8466cb8c63ad94
felipeseolin <seolin09>
2019-11-16 12:39:32 -0300

    Fixed HTTPCode

--- src/utils/HTTPCode.js
+++ src/utils/HTTPCode.js
@@ -1,14 +1,6 @@
 module.exports = {
-  OK() {
-    return 200;
-  },
-  CREATED() {
-    return 201;
-  },
-  BAD_REQUEST() {
-    return 400;
-  },
-  NOT_FOUND() {
-    return 404;
-  },
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
 };
