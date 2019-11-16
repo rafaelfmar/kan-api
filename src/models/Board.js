5897a51097b872dbda42acbb24b13c8e68512079
felipeseolin <seolin09>
2019-11-16 12:39:44 -0300

    Board complete API

--- src/models/Board.js
+++ src/models/Board.js
@@ -18,23 +18,3 @@ const BoardSchema = new Schema({
 }, { timestamps: true });
 
 mongoose.model('Board', BoardSchema);
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-
-const BoardSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  lists: [{
-    type: Schema.Types.ObjectId,
-    ref: 'List',
-  }],
-}, { timestamps: true });
-
-mongoose.model('Board', BoardSchema);
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,16 @@
 const express = require('express');
+
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.send("Kan API"));
+const BoardController = require('./controllers/BoardController');
+
+routes.get('/', (req, res) => res.send('Kan API'));
+
+// Board
+routes.get('/boards', BoardController.index);
+routes.post('/boards', BoardController.store);
+routes.get('/boards/:id', BoardController.show);
+routes.patch('/boards/:id', BoardController.update);
+routes.delete('/boards/:id', BoardController.destroy);
 
 module.exports = routes;
