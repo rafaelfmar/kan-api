c4b0f8dec21eacf08dd13835432a85ac0aebc458
felipeseolin <felipe.seolin@hotmail.com>
2019-11-26 16:37:26 -0300

    Refactor App.js

--- src/app.js
+++ src/app.js
@@ -4,20 +4,31 @@ const cors = require('cors');
 const routes = require('./routes');
 // Use .env file config
 require('dotenv').config();
+class AppController {
+  constructor() {
+    this.express = express();
+    this.express.set('port', process.env.PORT || 5000);
 
-const app = express();
+    this.middlewares();
+    this.routes();
+    this.database();
+  }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+  middlewares() {
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+    this.express.use(cors());
+  }
 
-app.use(cors());
+  routes() {
+    this.express.use('/api', routes);
+  }
 
-mongoose.connect(process.env.DB_URL, {
-  useUnifiedTopology: true,
-});
+  database() {
+    mongoose.connect(process.env.DB_URL, {
+      useUnifiedTopology: true,
+    });
+  }
+}
 
-app.use('/api', routes);
-
-app.set('port', process.env.PORT || 5000);
-
-module.exports = app;
+module.exports = new AppController().express;
